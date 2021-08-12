60e6e0c07bae3d79333a10185628d6e303053bd3
Yuto20 <88032726+Yuto20@users.noreply.github.com>
2021-08-12 15:01:52 +0900

    Revert "データベース設計"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'yuto.s' && password == '117819'
-    end
-  end
 end
