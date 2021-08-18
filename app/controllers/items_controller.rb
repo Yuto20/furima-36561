602bad1062f9010f14a28df404c0e6fc683cceee
Yuto20 <yuto1178@gmail.com>
2021-08-18 17:43:06 +0900

    get_item_paramsにupdateを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :get_item_params, only: [:show, :edit]
+  before_action :get_item_params, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -47,9 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if @item.user == current_user
-      render :edit
-    else
+    if @item.user != current_user
       redirect_to root_path
     end
   end
