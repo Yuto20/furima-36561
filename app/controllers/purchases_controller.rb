3f7adfddc26a5a35f950bc64bf8e1710f34db91e
Yuto20 <yuto1178@gmail.com>
2021-08-22 15:13:48 +0900

    購入のためのフォームの実装とそのフォームの情報を保存する処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,22 @@
 class PurchasesController < ApplicationController
+
   def index
-    
+    @purcahse_residence = PurchaseResidence.new
   end
 
   def create
-    
+    @purcahse_residence = PurchaseResidence.new(purcahse_params)
+    if @purcahse_residence.valid?
+      @purcahse_residence.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purcahse_params
+    binding.pry
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_num, :item_id).merge(user_id: current_user.id)
   end
 end
